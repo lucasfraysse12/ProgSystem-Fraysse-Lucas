25e2cd24479f5e7f2be31ebc98a74086a5a1ffaa
LUCAS FRAYSSE <lucas.fraysse@iut.local>
2026-09-08 16:24:30 +0200

    Algo image.java

---
+++ LectureEcritureJava/Image.java
@@ -0,0 +1,60 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Image {
+    private int width;
+    private int height;
+    // pixels[y][x][0=R,1=G,2=B]
+    private int[][][] pixels; // pixels[y][x][0=R,1=G,2=B]
+
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+
+    /**
+     * Constructeur : initialise une image vide.
+     */
+    public Image(int width, int hauteur) {
+        this.width = width;
+        this.height = height;
+        pixels = new int[hauteur][largeur][3];
+    }
+
+    /**
+     * Définit la couleur d'un pixel à la position (x, y)
+     */
+    public void setPixel(int x, int y, int r, int g, int b) {
+        if (x >= 0 && x < largeur && y >= 0 && y < hauteur) {
+            pixels[y][x][0] = r;
+            pixels[y][x][1] = g;
+            pixels[y][x][2] = b;
+        }
+    }
+
+    /**
+     * Sauvegarde l'image au format texte PPM (P3)
+     */
+    public void save_txt(String filename) throws IOException {
+        try {
+            FileWriter writer = new FileWriter(filename);
+
+            writer.write("P3\n");
+            // Écriture des dimensions
+			writer.write(largeur + " " + hauteur + "\n");
+            // Écriture de la valeur maximal
+			writer.write("255\n");
+            // Écriture des pixels
+            for (int y = 0; y < hauteur; y++) {
+				for (int x = 0; x < largeur; x++) {
+					writer.write(pixels[y][x][0] + " " + pixels[y][x][1] + " " + pixels[y][x][2] + " ");
+				}
+				writer.write("\n");
+			}
+
+            writer.close(); // Fermeture du fichier
+
+            System.out.println("Image PPM créée avec succès !");
+        } catch (IOException e) {
+            System.err.println("Erreur lors de l'écriture du fichier : " + e.getMessage());
+        }
+    }
+}
