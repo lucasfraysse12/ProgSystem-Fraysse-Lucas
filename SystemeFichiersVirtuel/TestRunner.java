1d3e8c1084d27a58192c717022c88fc7177f8d4a
Lucas Fraysse <lucas.fraysse@iut-rodez.fr>
2026-09-25 20:47:44 +0200

    Etape 4 - finie

--- SystemeFichiersVirtuel/TestRunner.java
+++ SystemeFichiersVirtuel/TestRunner.java
@@ -84,10 +84,57 @@ public class TestRunner {
         System.out.println("[OK] Étape 3 validée !");
     }
 
+    public static void testStep4() {
+        System.out.println("=== TEST ÉTAPE 4 : Initialisation Mémoire ===");
+
+        MemoryManager mm = new MemoryManager();
+
+        byte[] mem = mm.getFilesystemMemory();
+
+        assert mem != null :
+                "La mémoire ne doit pas être nulle";
+
+        assert mem.length == MemoryManager.TOTAL_MEMORY :
+                "Taille mémoire incorrecte";
+
+        assert Utils.readString(
+                mem,
+                MemoryManager.SUPERBLOCK_OFFSET,
+                16).equals("MYFS1.0") :
+                "Signature du superbloc incorrecte";
+
+        assert Utils.readInt(
+                mem,
+                MemoryManager.SUPERBLOCK_OFFSET + 16)
+                == MemoryManager.BLOCK_SIZE :
+                "Taille de bloc incorrecte";
+
+        assert Utils.readInt(
+                mem,
+                MemoryManager.SUPERBLOCK_OFFSET + 20)
+                == MemoryManager.TOTAL_MEMORY :
+                "Taille mémoire incorrecte";
+
+        assert Utils.readInt(
+                mem,
+                MemoryManager.SUPERBLOCK_OFFSET + 24)
+                == MemoryManager.NUM_BLOCKS :
+                "Nombre de blocs incorrect";
+
+        assert Utils.readInt(
+                mem,
+                MemoryManager.SUPERBLOCK_OFFSET + 28)
+                == MemoryManager.MAX_INODES :
+                "Nombre maximal d'inodes incorrect";
+
+        System.out.println("[OK] Étape 4 validée !");
+    }
+
 
     public static void main(String[] args) {
         //testStep2();
-        testStep3();
+        //testStep3();
+        testStep4();
     }
 
 }
